5d0192ae453636e601b23aa936a6414239310ede
Carlos <carlos@tresipunt.com>
2017-11-10 22:45:20 +0100

    Change package name Change icon app

---
+++ data/src/main/java/cat/cbonache/tfg/data/repository/datasource/Identifiable.kt
@@ -0,0 +1,9 @@
+package cat.cbonache.tfg.data.repository.datasource
+
+/**
+ * Created by Borja on 30/3/17.
+ */
+interface Identifiable<out Key> {
+
+    fun getKey(): Key
+}
---
+++ data/src/main/java/cat/cbonache/tfg/data/repository/datasource/TimeProvider.kt
@@ -0,0 +1,9 @@
+package cat.cbonache.tfg.data.repository.datasource
+
+/**
+ * Created by Borja on 30/3/17.
+ */
+interface TimeProvider {
+    fun currentTimeMillis(): Long
+
+}
