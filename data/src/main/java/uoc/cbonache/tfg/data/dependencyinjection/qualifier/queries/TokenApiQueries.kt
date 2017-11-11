2d2074a5283dcbf2b03d168ef51cbbb8b7363646
Carlos <carlos@tresipunt.com>
2017-11-11 22:32:34 +0100

    Token Queries

--- data/src/main/java/uoc/cbonache/tfg/data/dependencyinjection/qualifier/queries/TokenApiQueries.kt
+++ data/src/main/java/uoc/cbonache/tfg/data/dependencyinjection/qualifier/queries/TokenApiQueries.kt
@@ -3,8 +3,8 @@ package uoc.cbonache.tfg.data.dependencyinjection.qualifier.queries
 import javax.inject.Qualifier
 
 /**
- * Created by Borja on 3/4/17.
+ * @author cbonache
  */
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
-annotation class DefaultQueries
+annotation class TokenApiQueries
