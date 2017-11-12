823a3a5e084e86b594a667bc63664bcd95309307
Carlos <carlos@tresipunt.com>
2017-11-12 20:43:51 +0100

    Update Data

--- data/src/main/java/cat/cbonache/tfg/data/repository/Query+CheckInterface.kt
+++
@@ -1,9 +0,0 @@
-package cat.cbonache.tfg.data.repository
-
-import cat.cbonache.tfg.data.repository.query.Query
-
-/**
- * Created by Borja on 21/3/17.
- */
-
-fun Query.implements(kInterface: Class<*>): Boolean = kInterface.isAssignableFrom(this::class.java)
--- data/src/main/java/cat/cbonache/tfg/data/repository/datasource/TimeProvider.kt
+++
@@ -1,9 +0,0 @@
-package cat.cbonache.tfg.data.repository.datasource
-
-/**
- * Created by Borja on 30/3/17.
- */
-interface TimeProvider {
-    fun currentTimeMillis(): Long
-
-}
