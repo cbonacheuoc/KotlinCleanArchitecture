1f32162e0d6ce4360669aef968e6aff7aa2227e1
Carlos <carlos@tresipunt.com>
2017-11-12 19:57:50 +0100

    Add Base

---
+++ app/src/main/java/uoc/cbonache/tfg/dependencyinjection/qualifier/Base.kt
@@ -0,0 +1,10 @@
+package uoc.cbonache.tfg.dependencyinjection.qualifier
+
+import javax.inject.Qualifier
+
+/**
+ * @author cbonache
+ */
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Base
