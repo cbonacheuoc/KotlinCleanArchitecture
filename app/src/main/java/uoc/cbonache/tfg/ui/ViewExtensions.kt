497e6e46dbeb45aceb784eddd9995553f7326f45
Carlos <carlos@tresipunt.com>
2017-11-17 20:30:27 +0100

    App Bold Text in shipping list

--- app/src/main/java/uoc/cbonache/tfg/ui/ViewExtensions.kt
+++ app/src/main/java/uoc/cbonache/tfg/ui/ViewExtensions.kt
@@ -1,12 +1,28 @@
 package uoc.cbonache.tfg.ui
 
+//import android.content.Context
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.style.StyleSpan
+//import android.util.TypedValue
 import android.widget.ImageView
+import android.widget.TextView
 import com.squareup.picasso.Picasso
 
 /**
  * Created by Borja on 6/6/17.
  */
-
+/**
+ * @author cbonache
+ */
 fun ImageView.bind(url: String) {
     Picasso.with(this.context).load(url).into(this)
+}
+
+fun TextView.setPrefixTextBold(prefix: String, text: String, separator: String = " ") {
+    val styledText = SpannableString(prefix)
+    styledText.setSpan(StyleSpan(Typeface.BOLD),0,prefix.length,0)
+    this.append(styledText)
+    this.append(separator)
+    this.append(text)
 }
