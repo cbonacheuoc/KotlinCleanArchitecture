1cebc0f1b1485fdd3d5fff2bb39722e3543f4cbb
Carlos <carlos@tresipunt.com>
2017-12-01 13:16:54 +0100

    Update Navigator

--- app/src/main/java/uoc/cbonache/tfg/ui/Navigator.kt
+++ app/src/main/java/uoc/cbonache/tfg/ui/Navigator.kt
@@ -1,8 +1,13 @@
 package uoc.cbonache.tfg.ui
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import uoc.cbonache.tfg.ui.login.LoginActivity
 import uoc.cbonache.tfg.ui.shippingList.ShippingListActivity
+import uoc.cbonache.tfg.ui.shippingDetail.ShippingDetailActivity
+import uoc.cbonache.tfg.ui.shippingSign.ShippingSignActivity
+import uoc.cbonache.tfg.ui.shippingMap.ShippingMapActivity
 import javax.inject.Inject
 
 /**
@@ -11,7 +16,6 @@ import javax.inject.Inject
 class Navigator @Inject constructor() {
 
     fun navigateToShippingList(context: Context, flags: ArrayList<Int> = arrayListOf()) {
-
         val intent = ShippingListActivity.getIntent(context)
         for (flag in flags) {
             intent.addFlags(flag)
@@ -25,7 +29,26 @@ class Navigator @Inject constructor() {
     }
 
     fun navigateToShippingDetail(context: Context, idShipping: Long) {
+        val intent = ShippingDetailActivity.getIntent(context)
+        intent.putExtra(ShippingDetailActivity.ID_SHIPPING,idShipping)
+        context.startActivity(intent)
+    }
 
+    fun navigateToShippingSign(context: Context, idShipping: Long) {
+        val intent = ShippingSignActivity.getIntent(context)
+        intent.putExtra(ShippingSignActivity.ID_SHIPPING,idShipping)
+        context.startActivity(intent)
+    }
+
+    fun navigateToShippingMap(context: Context, idShipping: Long) {
+        val intent = ShippingMapActivity.getIntent(context)
+        intent.putExtra(ShippingMapActivity.ID_SHIPPING,idShipping)
+        context.startActivity(intent)
+    }
+
+    fun navigateToGoogleMapsApps(context: Context, destination: String){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?daddr=" + destination))
+        context.startActivity(intent)
     }
 
 }
