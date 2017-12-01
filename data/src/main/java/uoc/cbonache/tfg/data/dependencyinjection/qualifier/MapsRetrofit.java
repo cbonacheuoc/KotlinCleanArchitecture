3d6dd0bb87d44873fe3e5c9b8368fee5452898df
Carlos <carlos@tresipunt.com>
2017-12-01 13:08:29 +0100

    Add Maps dependecyinjection

---
+++ data/src/main/java/uoc/cbonache/tfg/data/dependencyinjection/qualifier/MapsRetrofit.java
@@ -0,0 +1,12 @@
+package uoc.cbonache.tfg.data.dependencyinjection.qualifier;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/*
+Created by Helm  25/1/17.
+*/
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MapsRetrofit {}
