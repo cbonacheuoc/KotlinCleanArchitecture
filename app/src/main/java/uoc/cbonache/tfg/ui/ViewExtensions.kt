daca1db4adb1d37f2d66b15927bd21e85cce98ac
Carlos <carlos@tresipunt.com>
2017-12-01 13:17:13 +0100

    Update App ViewExtensions

--- app/src/main/java/uoc/cbonache/tfg/ui/ViewExtensions.kt
+++ app/src/main/java/uoc/cbonache/tfg/ui/ViewExtensions.kt
@@ -1,10 +1,10 @@
 package uoc.cbonache.tfg.ui
 
-//import android.content.Context
+import android.content.Context
 import android.graphics.Typeface
 import android.text.SpannableString
 import android.text.style.StyleSpan
-//import android.util.TypedValue
+import android.util.TypedValue
 import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
@@ -17,6 +17,11 @@ import com.squareup.picasso.Picasso
  */
 fun ImageView.bind(url: String) {
     Picasso.with(this.context).load(url).into(this)
+}
+
+fun Int.toDp(context: Context): Int {
+    val metrics = context.resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
 }
 
 fun TextView.setPrefixTextBold(prefix: String, text: String, separator: String = " ") {
