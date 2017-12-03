97bd5c5b7bde6276a999cc7efa02d1ec1af63d09
Carlos <carlos@tresipunt.com>
2017-12-03 14:09:16 +0100

    Add Shipping State Queries

---
+++ data/src/main/java/uoc/cbonache/tfg/data/dependencyinjection/qualifier/queries/ShippingStateQueries.kt
@@ -0,0 +1,7 @@
+package uoc.cbonache.tfg.data.dependencyinjection.qualifier.queries
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ShippingStateQueries
